390d932e4e64b50a4f55230cb4c98206fcb14ad8
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2020-06-19 20:36:23 -0300

    import global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 
+import GlobalStyles from './styles/GlobalStyles'
+
 function App() {
   return (
-    <h2>Test</h2>
+
+    <>
+      <h2>Test</h2>
+
+      <GlobalStyles  />
+    </>
+
   );
 }
 
