814b2e94a778da1b44dc2ac57d9c8fed557ad536
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2020-06-20 19:06:00 -0300

    server name

---
+++ src/components/ServerName/styles.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    aling-items: center;
+    justify-content: space-between;
+
+    padding: 0 11px 0 16px;
+
+    background-color: var(--secondary);
+
+    box-shadow: rgba(0, 0, 0, 0.2) 0px 1px 0px 0px;
+    z-index: 2;
+` 
+
+export const Title = styled.div`
+    font-size: 16px;
+    font-weight: bold;
+    color: var(--white);
+
+    
+`
+
+export const ExpandIcon = styled.div`
+
+
+
+
+` 
