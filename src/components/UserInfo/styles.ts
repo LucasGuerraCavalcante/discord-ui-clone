98c46521cd357fabbd9f46924be94bc34e84110b
Lucas Guerra <lucasguerratee@gmail.com>
2020-06-23 15:51:47 -0300

    correcting channel info styles

--- src/components/UserInfo/styles.ts
+++ src/components/UserInfo/styles.ts
@@ -2,7 +2,43 @@ import styled from 'styled-components';
 import { Hashtag } from 'styled-icons/heroicons-outline';
 
 export const Container = styled.div`
+    grid-area: CI;
 
+    display: flex;
+    align-items: center;
+
+    padding: 0 17px;
+
+    background-color: var(--primary);
+    box-shadow: rgba(0, 0, 0, 0.2) 0px 1px 0px 0px;
+    z-index: 2;
 `
 
+export const Profile = styled.div` 
 
+
+`        
+export const Avatar = styled.div` 
+
+
+`
+export const UserData = styled.div` 
+
+
+`
+export const Icons = styled.div` 
+
+
+`
+export const MicIcon = styled.div` 
+
+
+`      
+export const HeadphoneIcon = styled.div` 
+
+
+`
+export const SettingsIcon = styled.div` 
+
+
+`
