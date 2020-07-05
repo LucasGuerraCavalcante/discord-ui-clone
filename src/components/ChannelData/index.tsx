fbaff334a896030495c00295e1a7ba2d66c09384
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2020-07-04 21:28:55 -0300

    ChannelData component

---
+++ src/components/ChannelData/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import ChannelMessage, { Mention } from '../ChannelMessage';
+
+import { Container, Messages, InputWrapper, Input, InputIcon } from './styles';
+
+const ChannelData: React.FC = () => {
+  return (
+    <Container>
+      <Messages >
+        {
+          Array.from(Array(15).keys()).map((n) => (
+            <ChannelMessage 
+              key={n}
+              author="Lucas Guerra"
+              date="04/07/2020"
+              content="Hey, I need to talk to you"
+            />
+          ))
+        }
+        <ChannelMessage 
+          author="Clone 1"
+          date="04/07/2020"
+          content={
+            <>
+              <Mention>@Lucas Guerra</Mention> OK, how can I serve you?
+            </>
+          }
+          hasMention
+          isBot
+        /> 
+      </ Messages>
+      <InputWrapper>
+        <Input type="text" placeholder="Chat in #clone-chat" />
+        <InputIcon />
+      </ InputWrapper>
+    </Container>
+  );
+};
+
+export default ChannelData;
